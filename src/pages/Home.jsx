4b8e909d6a8a22e28c3ab82261f1961b24d5bf0b
nurullah91 <mdnurullah.ptk@gmail.com>
2023-08-24 13:21:37 +0600

    inital setup done

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+             <h1 className="text-4xl text-center text-rose-500">Hello Redux  Home page</h1>
+           
+        </div>
+    );
+};
+
+export default Home;
